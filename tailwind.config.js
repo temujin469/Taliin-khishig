f3191d2ebbacf67720837f376b9de929c8b2d983
temujin469 <ttemujin469@gmail.com>
2022-12-15 10:56:50 +0800

    commited

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./pages/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#FE7200",
+      },
+    },
+  },
+  plugins: [],
+};
